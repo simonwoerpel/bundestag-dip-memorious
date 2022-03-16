2ef347d541d14c60d6a832bfeae76e2f098a9e46
Simon Wörpel <simon.woerpel@medienrevolte.de>
2022-03-17 00:15:14 +0100

    Handle START_DATE properly

--- src/bundestag_dip/operations.py
+++ src/bundestag_dip/operations.py
@@ -16,9 +16,9 @@ from .util import aleph_folder
 def init(context, data):
     f = furl(context.params["url"])
     if not env.to_bool("FULL_RUN"):
-        start_date = env.get("START_DATE", datetime.now() - timedelta(
+        start_date = env.get("START_DATE") or datetime.now() - timedelta(
             **ensure_dict(context.params.get("timedelta"))
-        ))
+        )
         f.args["f.datum.start"] = start_date.date().isoformat()
     data["url"] = f.url
     context.emit(data=data)
